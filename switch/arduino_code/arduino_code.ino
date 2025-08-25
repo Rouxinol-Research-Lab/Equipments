a55da5a2c02d1c176b5a985ffaf6e9ef87807227
Mariana Heimbecher Arias <117913527+AriasHe@users.noreply.github.com>
2025-08-25 09:11:52 -0300

    Repositories organization and update code

--- switch/arduino_code/arduino_code.ino
+++ switch/arduino_code/arduino_code.ino
@@ -3,7 +3,7 @@ int ligar = B0;
 int rele_ligar = B0;
 int dados = B1;
 int desligar = B1;
-int switchh, switch1=B000000,switch2=B000000;
+int switchh=0, switch1=B000000,switch2=B000000;
 int cmd,bobina;
 int liga_antiga = 0;
 int d_bobina_antiga_2,d_bobina_antiga_1,n_bobina_antiga_1,n_bobina_antiga_2;
@@ -18,10 +18,10 @@ void setup() {
    //EEPROM.write(3, 2);
   
  Serial.begin(9600);  
+
+
+  //Definição dos pinos para o controle de relés
   
-  //DDRF = B111111;
-  //DDRB = B111111; 
-  //0 = B111111;
   pinMode(13,OUTPUT);
   pinMode(12,OUTPUT);
   pinMode(11,OUTPUT);
@@ -37,6 +37,38 @@ void setup() {
   pinMode(A4,OUTPUT);
   pinMode(A5,OUTPUT);
 
+  //Definição dos pinos para controle dos leds
+  
+  pinMode(22,OUTPUT);
+  pinMode(24,OUTPUT);
+  pinMode(26,OUTPUT);
+  pinMode(28,OUTPUT);
+  pinMode(30,OUTPUT);
+  pinMode(32,OUTPUT);
+  
+  pinMode(34,OUTPUT);
+  pinMode(36,OUTPUT);
+  pinMode(38,OUTPUT);
+  pinMode(40,OUTPUT);
+  pinMode(42,OUTPUT);
+  pinMode(44,OUTPUT);
+
+  //Definição dos pinos para controle de botões
+  
+  pinMode(23,INPUT);
+  pinMode(25,INPUT);
+  pinMode(27,INPUT);
+  pinMode(29,INPUT);
+  pinMode(31,INPUT);
+  pinMode(33,INPUT);
+  
+  pinMode(35,INPUT);
+  pinMode(37,INPUT);
+  pinMode(39,INPUT);
+  pinMode(41,INPUT);
+  pinMode(43,INPUT);
+  pinMode(45,INPUT);
+  
   
 
   
@@ -48,25 +80,46 @@ void setup() {
 void loop() {
   
   if (Serial.available() > 0) {
+     cmd = Serial.read();    
+   }else{
+        cmd =  (digitalRead(23)==1  ? 11 : digitalRead(25)==1  ? 12 : digitalRead(27)==1  ? 13 : digitalRead(29)==1  ? 14 : digitalRead(31)==1  ? 15 : digitalRead(33)==1  ? 16 : digitalRead(35)==1  ? 21 : digitalRead(37)==1  ? 22 : digitalRead(39)==1  ? 23 : digitalRead(41)==1  ? 24 : digitalRead(43)==1  ? 25 : digitalRead(45)==1  ? 26 : 0 );
+     
+   }
+   if(cmd== 42){
+      Serial.println(EEPROM.read(1),DEC);
+      Serial.println(EEPROM.read(3),DEC);
+      
 
-     cmd = Serial.read();
+   }else{
+      if(cmd==10){
+         desligar_todas_bobinas();
+      }else{
+        if(cmd!=0){
+        programa_principal();
+      }
+      }
+      
+   }
+  
+   liga_led();
+
+}
+ 
+
+void programa_principal(){
      switchh = cmd/10;
      bobina = cmd%10;
      aberto();
-     delay(1000);
+     delay(50);
      desligar_bobina();
-     delay(1000);
+     delay(500);
      aberto();
-     delay(1000);
+     delay(50);
      escolher_bobina(bobina);
      ligar_bobina();
-     delay(1000);
+     delay(50);
      aterrado();
-     delay(1000);
-    
-    
-   }
-
+     delay(50);   
 }
 
 void escolher_bobina(int valor){
@@ -108,6 +161,7 @@ void ligar_bobina(){
      
      delay(10);
      switchhh(dados);
+     
  
 
 }
@@ -141,6 +195,37 @@ void desligar_bobina(){
      delay(10);
 }
 
+void desligar_todas_bobinas(){
+
+for(int sw=1;sw<=2;sw=sw+1){
+  for(int var=1;var<=6;var=var+1){
+    escolher_bobina(var);
+      if(var%2==0){
+        digitalWrite(7,0);
+        digitalWrite(6,1); 
+      }else{
+        digitalWrite(7,1);
+        digitalWrite(6,0); 
+      } 
+      if(sw==1){ 
+        passar_valores_b(dados);
+        passar_valores_c(B111111);
+      }else{
+        passar_valores_b(B111111);
+        passar_valores_c(dados);
+      }
+        delay(200); 
+  }
+  delay(1000);
+}
+  EEPROM.update(1,0);
+  EEPROM.update(3,0);
+
+  Serial.println(EEPROM.read(1),DEC);
+  Serial.println(EEPROM.read(3),DEC);
+
+
+}
 
 
 void switchhh(int valor){
@@ -179,12 +264,12 @@ void aterrado(){
 } 
 
 void passar_valores_c(int valor){
-    digitalWrite(A0, valor & (1 << 5)  ? 1 : 0);
-    digitalWrite(A1, valor & (1 << 4)  ? 1 : 0);
-    digitalWrite(A2, valor & (1 << 3)  ? 1 : 0);
-    digitalWrite(A3, valor & (1 << 2)  ? 1 : 0);
-    digitalWrite(A4, valor & (1 << 1)  ? 1 : 0);
-    digitalWrite(A5, valor & (1 << 0)  ? 1 : 0);
+    digitalWrite(A5, valor & (1 << 5)  ? 1 : 0);
+    digitalWrite(A4, valor & (1 << 4)  ? 1 : 0);
+    digitalWrite(A3, valor & (1 << 3)  ? 1 : 0);
+    digitalWrite(A2, valor & (1 << 2)  ? 1 : 0);
+    digitalWrite(A1, valor & (1 << 1)  ? 1 : 0);
+    digitalWrite(A0, valor & (1 << 0)  ? 1 : 0);
 
   
 }
@@ -196,4 +281,20 @@ void passar_valores_b(int valor){
     digitalWrite(10, valor & (1 << 2)  ? 1 : 0);
     digitalWrite( 9, valor & (1 << 1)  ? 1 : 0);
     digitalWrite( 8, valor & (1 << 0)  ? 1 : 0);    
+}
+
+void liga_led(){
+    digitalWrite(22, EEPROM.read(1) == 1 ? 1 : 0); 
+    digitalWrite(24, EEPROM.read(1) == 2 ? 1 : 0); 
+    digitalWrite(26, EEPROM.read(1) == 3 ? 1 : 0);
+    digitalWrite(28, EEPROM.read(1) == 4 ? 1 : 0);
+    digitalWrite(30, EEPROM.read(1) == 5 ? 1 : 0); 
+    digitalWrite(32, EEPROM.read(1) == 6 ? 1 : 0);
+    
+    digitalWrite(34, EEPROM.read(3) == 1 ? 1 : 0);
+    digitalWrite(36, EEPROM.read(3) == 2 ? 1 : 0); 
+    digitalWrite(38, EEPROM.read(3) == 3 ? 1 : 0);
+    digitalWrite(40, EEPROM.read(3) == 4 ? 1 : 0);
+    digitalWrite(42, EEPROM.read(3) == 5 ? 1 : 0); 
+    digitalWrite(44, EEPROM.read(3) == 6 ? 1 : 0);
 }
